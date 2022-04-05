d6844786d0b1051e72f75a3b7f9d337e1a614dc0
Michael Eldred <michaelreldred@gmail.com>
2022-04-05 09:34:41 -0600

    Basic functionality working for add card states

--- src/Column/AddCardButton.test.tsx
+++ src/Column/AddCardButton.test.tsx
@@ -1,11 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { AddCardButton } from './AddCardButton';
 
 describe("Add Card Button", () => {
-    it("changes color when hovered", () => {
-        
+    it('renders a button but default', () => {
+        render(<AddCardButton />);
+        expect(screen.getAllByRole("button")).toHaveLength(1);
     });
-  
-    it("Adds a new card when clicked", () => {
-  
+
+    it('changes to a textbox when clicked', () => {
+        render(<AddCardButton />);
+        fireEvent.click(screen.getByRole('button'));
+        expect(screen.getAllByRole("textbox")).toHaveLength(1);
+    });
+
+    it('resets when enter is pressed', () => {
+        render(<AddCardButton />);
+
+        fireEvent.click(screen.getByRole('button'));
+        fireEvent.keyDown(screen.getByRole("textbox"), {
+            key: "Enter",
+            shiftKey: false
+        });
+
+        expect(screen.queryAllByRole("textbox")).toHaveLength(0);
+        expect(screen.getAllByRole("button")).toHaveLength(1);
     });
   })
--- src/Column/AddCardButton.tsx
+++ src/Column/AddCardButton.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 export const AddCardButton: React.FunctionComponent<AddCardButtonProps> = props => {
-    return (
-        <button className="card">+</button>
-    );
+    const [isCreating, setIsCreating] = useState<Boolean>(false);
+
+    const checkForOnEnter = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        if(e.key === "Enter" && e.shiftKey === false) {
+            setIsCreating(false);
+            e.preventDefault()
+        }
+    }
+
+    if(isCreating) {
+        return (
+            <div className="card">
+                <textarea onKeyDown={checkForOnEnter}/>
+            </div>
+        )
+    } else {
+        return (
+            <button className="card" onClick={() => {setIsCreating(true)}}>+</button>
+        );
+    }
 }
 
 export type AddCardButtonProps = {
